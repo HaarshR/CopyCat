35d18f92c77e27e33e2602ae6792e72c1d12f56f
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-12-03 21:07:19 +0400

    added reset grid and generate button with attributes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const gridSizeInput = document.getElementById("grid-size");
 const buildGridButton = document.getElementById("build-grid");
+const resetGridButton = document.getElementById("reset-grid");
 
 let gridSize = 0; // User Chooses
-const BOX_SIZE = 100;
+const BOX_SIZE = 100; // Fixed button size
+
+let generated = false;
 
 const computerGridContainer = document.getElementById(
   "computer-grid-container"
@@ -10,7 +13,9 @@ const computerGridContainer = document.getElementById(
 const userGridContainer = document.getElementById("user-grid-container");
 
 const computerGridContainerStyle = computerGridContainer.style;
+computerGridContainerStyle.display = "grid";
 const userGridContainerStyle = userGridContainer.style;
+userGridContainerStyle.display = "grid";
 
 const generateGrid = (component, parentComponent) => {
   component.style.width = BOX_SIZE;
@@ -18,7 +23,7 @@ const generateGrid = (component, parentComponent) => {
   parentComponent.appendChild(component);
 };
 
-const generateButton = (classList) => {
+const generateButton = (classList, attributeList) => {
   let button = document.createElement("button");
 
   if (classList.length > 0) {
@@ -27,33 +32,54 @@ const generateButton = (classList) => {
     });
   }
 
+  if (attributeList) {
+    if (attributeList.length > 0) {
+      attributeList.forEach(({ attribute, value }) => {
+        button.setAttribute(attribute, value);
+      });
+    }
+  }
+
   return button;
 };
 
 const buildGrid = () => {
-  gridSize = gridSizeInput.value;
-  let repeatCSS = `repeat(${gridSize}, ${BOX_SIZE}px)`;
+  if (!generated) {
+    gridSize = gridSizeInput.value;
+    let gridCount = gridSize * gridSize;
+    let repeatCSS = `repeat(${gridSize}, ${BOX_SIZE}px)`;
 
-  computerGridContainerStyle.display = "grid";
-  computerGridContainerStyle.gridTemplateColumns = repeatCSS;
-  computerGridContainerStyle.gridTemplateRows = repeatCSS;
+    computerGridContainerStyle.gridTemplateColumns = repeatCSS;
+    computerGridContainerStyle.gridTemplateRows = repeatCSS;
 
-  userGridContainerStyle.display = "grid";
-  userGridContainerStyle.gridTemplateColumns = repeatCSS;
-  userGridContainerStyle.gridTemplateRows = repeatCSS;
+    userGridContainerStyle.gridTemplateColumns = repeatCSS;
+    userGridContainerStyle.gridTemplateRows = repeatCSS;
 
-  if (gridSize >= 2) {
-    for (let i = 0; i < gridSize * gridSize; i++) {
-      let computerButton = generateButton(["btn", "btn-primary", "m-1"]);
-      let userButton = generateButton(["btn", "btn-success", "m-1"]);
+    if (gridSize >= 2) {
+      for (let i = 0; i < gridCount; i++) {
+        let computerButton = generateButton(
+          ["btn", "btn-primary", "m-1"],
+          [{ attribute: "disabled", value: true }]
+        );
+        let userButton = generateButton(["btn", "btn-success", "m-1"]);
 
-      generateGrid(computerButton, computerGridContainer);
-      generateGrid(userButton, userGridContainer);
+        generateGrid(computerButton, computerGridContainer);
+        generateGrid(userButton, userGridContainer);
+      }
     }
   }
+
+  generated = true;
+};
+
+const resetGrid = () => {
+  generated = false;
+  computerGridContainer.textContent = "";
+  userGridContainer.textContent = "";
 };
 
 buildGridButton.addEventListener("click", buildGrid);
+resetGridButton.addEventListener("click", resetGrid);
 
 const main = () => {
   //   buildGrid();
